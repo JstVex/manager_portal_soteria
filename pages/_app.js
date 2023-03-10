1b62bd1bab116fddf1a5c5f13962c47d4450518e
JstVex <heinhtetlulu14@gmail.com>
2023-03-10 14:40:48 +0630

    Create navbar using composition

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,75 @@
 import '@/styles/globals.css'
-import Navbar from '@/components/Navbar'
+// import Navbar from '@/components/Navbar'
+import { useState } from 'react'
+import Link from 'next/link'
+import { IoIosArrowDown } from 'react-icons/io'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Navbar />
+      <Navbar>
+        <div className='title'>
+          Manager Portal
+        </div>
+
+        <NavItem text={'New'}>
+          <DropdownMenu route={'new'}>
+          </DropdownMenu>
+        </NavItem>
+
+        <NavItem text={'Outdated'}>
+          <DropdownMenu route={'outdated'}>
+          </DropdownMenu>
+        </NavItem>
+
+        <div className='login'>
+          Login
+        </div>
+      </Navbar>
       <Component {...pageProps} />
     </>
 
+  )
+}
+
+function Navbar(props) {
+  return (
+    <nav className='navbar'>
+      <ul className='navbar-nav'>
+        {props.children}
+      </ul>
+    </nav>
+  )
+}
+
+function NavItem(props) {
+
+  const [open, setOpen] = useState(false);
+
+  return (
+    <li className="nav-item" onClick={() => setOpen(!open)}>
+      {props.text}
+      <IoIosArrowDown className='toggleicon' />
+
+      {open && props.children}
+    </li>
+  )
+}
+
+function DropdownMenu(props) {
+
+  function DropdownItem(props) {
+    return (
+      <Link href={props.link} className="menu-item">
+        {props.children}
+      </Link>
+    )
+  }
+
+  return (
+    <div className="dropdown">
+      <DropdownItem link={`/${props.route}/donations`}>Donation</DropdownItem>
+      <DropdownItem link={`/${props.route}/campaigns`}>Campaign</DropdownItem>
+    </div>
   )
 }
