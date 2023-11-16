dbc36b59052103305c14f4c98b70bde80ba80899
JstVex <heinhtetlulu14@gmail.com>
2023-11-15 16:22:27 -0800

    Fix Navbar and dropdown feature

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,54 @@
-import Link from "next/link"
+import Link from "next/link";
+import { ChevronDown } from 'lucide-react';
+import { useContext } from "react";
+import { AuthContext } from "@/context/AuthContext";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
+    const { user, logout } = useContext(AuthContext);
+
+    const router = useRouter();
+
+    const handleLogout = () => {
+        logout();
+        router.push('/login');
+    }
+
     return (
-        <nav className="navbar-container">
-            <Link href='/' className="name">
+        <nav className="navbar">
+            <Link href='/' className='title'>
                 Manager Portal
             </Link>
-            <div className="routes">
-                <Link href='/new/donations'>New Donation</Link>
-                <Link href='/new/campaigns'>New Campaign</Link>
-                <Link href='/outdated/donations'>Outdated Donation</Link>
-                <Link href='/outdated/campaigns'>Outdated Campaign</Link>
-            </div>
-            <div>
-                Login
+            <div className="right">
+                <ul className="nav-links">
+                    <li className="nav-item">
+                        <div className='wrap'>
+                            <div>New</div>
+                            <ChevronDown size={20} />
+                        </div>
+                        <div className="dropdown">
+                            <Link href="/new/donations">Donations</Link>
+                            <Link href="/new/campaigns">Campaigns</Link>
+                        </div>
+                    </li>
+                    <li className="nav-item">
+                        <div className='wrap'>
+                            <div>Outdated</div>
+                            <ChevronDown size={20} />
+                        </div>
+                        <div className="dropdown">
+                            <Link href="/outdated/donations">Donations</Link>
+                            <Link href="/outdated/campaigns">Campaigns</Link>
+                        </div>
+                    </li>
+                    <li className="nav-item">
+                        {user ?
+                            <div onClick={handleLogout}>Logout</div>
+                            :
+                            <Link href="/login">Login</Link>
+                        }
+                    </li>
+                </ul>
             </div>
         </nav>
     )
--- pages/_app.js
+++ pages/_app.js
@@ -1,75 +1,12 @@
 import '@/styles/globals.css'
-// import Navbar from '@/components/Navbar'
-import { useState } from 'react'
-import Link from 'next/link'
-import { IoIosArrowDown } from 'react-icons/io'
+import Navbar from '@/components/Navbar'
+import { AuthContextProvider } from '@/context/AuthContext'
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Navbar>
-        <Link href='/' className='title'>
-          Manager Portal
-        </Link>
-
-        <NavItem text={'New'}>
-          <DropdownMenu route={'new'}>
-          </DropdownMenu>
-        </NavItem>
-
-        <NavItem text={'Outdated'}>
-          <DropdownMenu route={'outdated'}>
-          </DropdownMenu>
-        </NavItem>
-
-        <div className='login'>
-          Login
-        </div>
-      </Navbar>
+    <AuthContextProvider>
+      <Navbar />
       <Component {...pageProps} />
-    </>
-
-  )
-}
-
-function Navbar(props) {
-  return (
-    <nav className='navbar'>
-      <ul className='navbar-nav'>
-        {props.children}
-      </ul>
-    </nav>
-  )
-}
-
-function NavItem(props) {
-
-  const [open, setOpen] = useState(false);
-
-  return (
-    <li className="nav-item" onClick={() => setOpen(!open)}>
-      {props.text}
-      <IoIosArrowDown className='toggleicon' />
-
-      {open && props.children}
-    </li>
-  )
-}
-
-function DropdownMenu(props) {
-
-  function DropdownItem(props) {
-    return (
-      <Link href={props.link} className="menu-item">
-        {props.children}
-      </Link>
-    )
-  }
-
-  return (
-    <div className="dropdown">
-      <DropdownItem link={`/${props.route}/donations`}>Donation</DropdownItem>
-      <DropdownItem link={`/${props.route}/campaigns`}>Campaign</DropdownItem>
-    </div>
+    </AuthContextProvider>
   )
 }
