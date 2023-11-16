9830d098e524b2bb9c94fbddd1a3274878cf8b12
JstVex <heinhtetlulu14@gmail.com>
2023-11-15 16:25:30 -0800

    Fix home page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      hello
-
+      <div className={styles.wrapper}>
+        <div className={styles.title}>
+          Welcome to Manager Portal for <a target='_blank' href='http://localhost:3000' className={styles.app}>Soteria</a> Application
+        </div>
+        <div className={styles.text}>
+          It is to manage new and outdated posts for donations and campaigns.
+        </div>
+        <Link href='/login' className={styles.login}>
+          Login
+        </Link>
+      </div>
     </div >
   );
 }
