55cb08e5721ea685b6917f29d2e2236039cc092b
JstVex <heinhtetlulu14@gmail.com>
2023-11-16 00:04:33 -0800

    Change login to manage posts if user exists

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import styles from '@/styles/Home.module.css';
 import Link from 'next/link';
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
 
 export default function Home() {
+  const { user } = useContext(AuthContext);
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
@@ -11,9 +15,16 @@ export default function Home() {
         <div className={styles.text}>
           It is to manage new and outdated posts for donations and campaigns.
         </div>
-        <Link href='/login' className={styles.login}>
-          Login
-        </Link>
+        {user ? (
+          <Link href='/new/donations' className={styles.btn}>
+            Manage Posts
+          </Link>
+        ) : (
+          <Link href='/login' className={styles.login}>
+            Login
+          </Link>
+        )
+        }
       </div>
     </div >
   );
